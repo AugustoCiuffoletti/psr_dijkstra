062d94e9f5cec4894df736fd7dd628b04b9d6f22
Augusto Ciuffoletti <augusto@di.unipi.it>
2017-03-30 16:14:02 +0200

    Algoritmo migliorato con il first hop

--- dijkstra.py
+++ dijkstra.py
@@ -3,19 +3,23 @@ from collections import defaultdict
 # Dato il grafo e la radice restituisce la tabella di routing
 # per la radice
 def dijkstra(nodi,archi,radice):
-    raggiunti = {radice: ("",0)}
+    raggiunti = {radice: ("",0,"")}
     next_hop = {}
     
-    # Il grafo e' definito dall'insieme "nodi" e dall'insieme "archi",
-    # coppie di nodi collegati da un link. La radice dell'albero (cioe'
+    # Alla funzione vengono passati il grafo della rete, definito
+    # dall'insieme "nodi" e dall'insieme "archi", che sono coppie 
+    # di nodi collegati da un link. La radice dell'albero (cioe'
     # il nodo che esegue l'algoritmo) e' nel parametro "radice".
+    #
     # L'algoritmo svuota progressivamente l'insieme dei "nodi" aggiungendo
-	# i nodi che rimuove al dizionario "raggiunti" e calcolandone la
+	# quelli che rimuove al dizionario "raggiunti" e calcolandone la
 	# distanza. Il dizionario "raggiunti" e' la tabella di routing,
-	# e associa alla destinazione una coppia (ultimo hop, distanza), e
-	# viene aggiornato con il risultato dell'algoritmo.
+	# e associa alla destinazione una tripla (ultimo hop, distanza,
+	# primo hop), e viene aggiornato con il risultato dell'algoritmo.
+	# Il primo elemento della tupla, l'ultimo hop, e' funzionale
+	# all'algoritmo, ma non e' rilevante ai fini del routing.
     
-    while nodi:      # Ripete finche' l'insieme "nodi" si svuota 
+    while nodi:      # Si arresta quando l'insieme "nodi" e' vuoto 
     # PREPARAZIONE
         # Trovo nodo con distanza minima tra quelli raggiunti
         nodo_min = None
@@ -40,7 +44,10 @@ def dijkstra(nodi,archi,radice):
 			nuova_distanza = distanza_min + distanze_da_min[destinazione]
             # Se migliore della precedente sostituisco distanza e prossimo
 			if destinazione not in raggiunti or nuova_distanza < raggiunti[destinazione][1]:
-				raggiunti[destinazione] = (nodo_min, nuova_distanza)
+				if raggiunti[nodo_min][2] == "":
+					raggiunti[destinazione] = (nodo_min, nuova_distanza, destinazione)
+				else:
+					raggiunti[destinazione] = (nodo_min, nuova_distanza, raggiunti[nodo_min][2])
     return raggiunti
 
 # 4 nodi: provare a disegnare la rete
@@ -55,7 +62,7 @@ def dijkstra(nodi,archi,radice):
 
 
 l = dijkstra(
-set(['1','2','3','4', '5', '6','7']),
+set(['1','2','3','4', '5', '6', '7']),
 {('1','2'):7,
  ('1','3'):9,
  ('1','6'):14,
